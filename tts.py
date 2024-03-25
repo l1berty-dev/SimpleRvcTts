bc58c9bbfbbfcb9697924b4d6f5c6e1c9d6631fa
L1berty <76559363+l1berty-dev@users.noreply.github.com>
2024-03-25 14:57:28 +0500

    Update tts.py

--- tts.py
+++ tts.py
@@ -18,7 +18,7 @@ class TTS():
     def get(self, tts_text):
         info, edge_output_filename, result = tts(self.model_name, self.model_root, self.speed, tts_text, self.tts_voice,
                                                  self.f0_up_key, self.f0_method, self.index_rate, self.protect)
-        return (result[0], result[1])
+        return info, (result[0], result[1])
     def play(self, data):
         sd.play(data[0], data[1])
         time.sleep(len(data[0]) / data[1])
